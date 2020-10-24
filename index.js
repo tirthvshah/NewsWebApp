8eecd68aea02dae92069ffeba0155ea437fc77ea
akash <akash.ry@somaiya.edu>
2020-10-24 17:00:41 +0530

    fetch top-headlines

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express=require('express');
 const bodyParser = require('body-parser');
 const app=express();
 const Searchroutes=require('./routes/search');
+const Headlineroute = require('./routes/headline')
+
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
+
+
+app.use('/search',Searchroutes);
+app.use('/headlines',Headlineroute)
+app.get('/',(req,res)=>{
+    res.redirect('/headlines')
+})
 app.listen('3000',() =>{
     console.log("Server started ");
 });
-app.use('/search',Searchroutes);
+
 module.exports = app;
