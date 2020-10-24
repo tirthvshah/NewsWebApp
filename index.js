dded76c1dbec1d776ad77bccae85392f3560289f
dakshit050 <dakshit.v@somaiya.edu>
2020-10-24 16:11:49 +0530

    Filtes added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const bodyParser = require('body-parser');
 const app=express();
-const Searchroutes=require('./routes/search');
+const filterroutes=require('./routes/filters');
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.listen('3000',() =>{
     console.log("Server started ");
 });
-app.use('/search',Searchroutes);
+app.use('/filters',filterroutes);
 module.exports = app;
