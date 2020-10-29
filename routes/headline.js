681a486c6e88b590abd0e1cc865e83e80326f7f4
akash <akash.ry@somaiya.edu>
2020-10-29 16:14:29 +0530

    fetch everything related to sports, business, entertainment,politics,technology

--- routes/headline.js
+++ routes/headline.js
@@ -3,11 +3,13 @@ const router= express.Router();
 const axios = require('axios')
 const { response } = require('..')
 
+
 router.get('/',(req,res)=>{
-    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=4b2821b99dd64a708f00564be388c26a`)
+    axios.get(`https://newsapi.org/v2/everything?q="sports"OR"business"OR"entertainment"OR"politics"OR"technology"&sortBy=publishedAt&pageSize=100&sortBy=relevancy&apiKey=4b2821b99dd64a708f00564be388c26a`)
     .then(response=>{
         if(response.status == 200){
             console.log(response.data.articles)
+            console.log(response.data.totalResults)
             res.render('headlines',{data:response.data.articles})
         }
     })
