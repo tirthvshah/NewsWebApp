59900e8b7bc25859026433bd74f82cc7fa96fe2a
akash <akash.ry@somaiya.edu>
2020-11-01 21:28:15 +0530

    Complete responsive UI

--- routes/headline.js
+++ routes/headline.js
@@ -4,8 +4,24 @@ const axios = require('axios')
 const { response } = require('..')
 
 
+// router.get('/',(req,res)=>{
+//     axios.get(`https://gnews.io/api/v4/search?q=Apple&lang=en&max=100&token=f7868935381cdd14750fe3e6b7ed3b5d`)
+//     .then(response=>{
+//         if(response.status == 200){
+//             console.log(response.data.articles)
+//             // arr = response.data.results.multimedia
+//             // arr.forEach(data=>{
+//                 // console.log(data)
+//             // })
+//             console.log(response.data.totalArticles)
+//             res.render('headlines',{data:response.data.articles})
+//         }
+//     })
+// })
+
 router.get('/',(req,res)=>{
-    axios.get(`https://newsapi.org/v2/everything?q="sports"OR"business"OR"entertainment"OR"politics"OR"technology"&sortBy=publishedAt&pageSize=100&sortBy=relevancy&apiKey=4b2821b99dd64a708f00564be388c26a`)
+    // axios.get(`https://newsapi.org/v2/everything?q="sports"OR"business"OR"entertainment"OR"politics"OR"technology"&sortBy=publishedAt&pageSize=100&sortBy=relevancy&apiKey=4b2821b99dd64a708f00564be388c26a`)
+    axios.get(`http://newsapi.org/v2/top-headlines?country=in&apiKey=4b2821b99dd64a708f00564be388c26a`)
     .then(response=>{
         if(response.status == 200){
             console.log(response.data.articles)
@@ -14,6 +30,7 @@ router.get('/',(req,res)=>{
         }
     })
 })
+
 
 
 module.exports = router;
